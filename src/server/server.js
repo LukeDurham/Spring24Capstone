2cfd5aff5037021c582fe7b97f4c93f107ca286e
LukeDurham <durham35@marshall.edu>
2024-02-26 17:48:27 -0500

    Created multiple web pages to prep for the api backend connection

---
+++ src/server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
